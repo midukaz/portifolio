422d19245b6736124ee84279c8d6e2e57617064e
midukaz <paulobarreto758@gmail.com>
2025-06-14 16:55:55 -0300

    Add custom domain configuration

--- svelte.config.js
+++ svelte.config.js
@@ -19,8 +19,8 @@ const config = {
 				strict: true
 		}),
 		paths: {
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			base: '',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			// base: '',
 			assets: ''
 		
 		}
