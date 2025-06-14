2cc3b662d92c0055c9d980121497b17955a12f9c
midukaz <paulobarreto758@gmail.com>
2025-06-14 13:19:47 -0300

    configuracao do adapter

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'app.html' // may differ from host to host
-		})
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
